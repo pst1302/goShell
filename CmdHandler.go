54ebd33f03504b73d81d04dde353951f68da5ff9
이길복 <igilbog@igilbog-ui-MacBookPro.local>
2021-12-03 15:13:44 +0900

    add ls cmd , but it isn't completed.

--- CmdHandler.go
+++ CmdHandler.go
@@ -2,18 +2,40 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
 	"strings"
 )
 
 func ParsingCmd(cmd string) {
-	fmt.Println("Parsing start with " + cmd)
+	//fmt.Println("Parsing start with " + cmd)
 
 	splitedCmd := strings.Split(cmd, " ")
 
-	fmt.Printf("length : %d\n", len(splitedCmd))
-
-	for i := 0; i < len(splitedCmd); i++ {
-
-		fmt.Println(splitedCmd[i])
+	if len(splitedCmd) == 0 {
+		return
 	}
+
+	if splitedCmd[0] == "ls\n" {
+		Ls_cmd_handler()
+	}
+}
+
+func Ls_cmd_handler() {
+	ex, err := os.Executable()
+	if err != nil {
+		//
+	}
+
+	exPath := filepath.Dir(ex)
+	files, err := ioutil.ReadDir(exPath)
+	if err != nil {
+		//
+	}
+
+	for _, file := range files {
+		fmt.Print(file.Name() + " ")
+	}
+	fmt.Println()
 }
