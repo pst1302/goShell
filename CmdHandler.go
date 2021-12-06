8b90f2bcadea379748631c0b4e60111229a608fb
Gilbok Lee <Gilbok.Lee@intelliantech.com>
2021-12-06 17:46:41 +0900

    ls command parameter add

--- CmdHandler.go
+++ CmdHandler.go
@@ -18,24 +18,76 @@ func ParsingCmd(cmd string) {
 	}
 
 	if splitedCmd[0] == "ls\n" {
-		Ls_cmd_handler()
+		Ls_cmd_handler("")
+	} else if splitedCmd[0] == "ls" {
+		if len(splitedCmd) == 2 {
+			Ls_cmd_handler(splitedCmd[1])
+		}
 	}
 }
 
-func Ls_cmd_handler() {
-	ex, err := os.Executable()
+func Ls_cmd_handler(addedString string) {
+
+	if addedString != "" {
+		if strings.HasPrefix(addedString, "-") {
+			// arg is option
+			fmt.Println("arg is option")
+		} else {
+			// arg is path
+			fmt.Println("arg is path : " + addedString)
+
+			fmt.Println(GetFilesFromDir(addedString))
+		}
+	} else {
+		ex, err := os.Executable()
+		if err != nil {
+			//
+		}
+
+		fmt.Println(GetFilesFromDir(ex))
+	}
+}
+
+func GetFilesFromDir(dir string) string {
+
+	dir = strings.ReplaceAll(dir, "\n", "")
+
+	fileInfo, err := os.Stat(dir)
 	if err != nil {
-		//
+		// error handling
+		fmt.Println("There is something problem")
+		if os.IsNotExist(err) {
+			dir += "/"
+			fileInfo, err := os.Stat(dir)
+
+			fmt.Println("dir ::: " + dir)
+			if err != nil {
+				return "There is some problem while reading directory"
+			}
+
+			if !fileInfo.IsDir() {
+				return "There is some problem while reading directory"
+			}
+		}
 	}
 
-	exPath := filepath.Dir(ex)
+	if fileInfo != nil {
+		fmt.Println("file Info isn't nil")
+		dir += "/"
+	}
+
+	exPath := filepath.Dir(dir)
+
 	files, err := ioutil.ReadDir(exPath)
 	if err != nil {
 		//
 	}
 
+	filesList := ""
+
 	for _, file := range files {
-		fmt.Print(file.Name() + " ")
+		filesList += fmt.Sprint(file.Name() + " ")
 	}
-	fmt.Println()
+
+	return filesList
 }
